747156f9ede4fc0121aa7b7819f51c2e1485ef24
bpoir <brianpoirier1@gmail.com>
2017-10-04 21:42:47 -0400

    Add files via upload

--- odd.py
+++ odd.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+import random
+import time
 
 odds = [1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35,37,39,41,43,45,47,
         49,51,53,55,57,59]
@@ -10,4 +12,6 @@ for i in range(5):
         print("This minute is odd")
     else:
         print("Not an odd minute")
-          
+
+    wait_time = random.randint(1,60)
+    time.sleep(wait_time)
